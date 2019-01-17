09ca49b3a60a811ce442744f456b0aebaf8d1788
gawareve <gawareve@rose-hulman.edu>
2019-01-17 09:44:26 -0500

    I completed m2

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -32,7 +32,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Notice the small horizontal BLUE bars on the scrollbar-like thing
 #   on the right.  Each blue bar indicates a thing TO-DO in this module.
 #
@@ -57,10 +57,10 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add another   print   statement below this comment.
 #   It should print any string that you want (but keep it polite and G-rated!)
-#
+print('it is really early')
 #   Test your code by re-running this module, either by proceeding
 #   as you did when you ran this module the first time,
 #   or by pressing the green rightward-pointing triangle
@@ -71,12 +71,12 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
 #   You do NOT have to use strings for this, so no quotation marks!
-#
+print(3607*34227)
 #   TEST your code by re-running this module, then asking someone
 #   whom you trust:
 #      What number did your   print   display for this TO-DO?
@@ -86,7 +86,7 @@ print('Hello, World')
 
 ###############################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py

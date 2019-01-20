c6561aca0be561ba014fdc58bc642608be5cdf70
gawareve <gawareve@rose-hulman.edu>
2019-01-20 14:55:39 -0500

    I completed m6

--- src/m7_summary.py
+++ src/m7_summary.py
@@ -2,16 +2,16 @@
 An exercise that summarizes what you have learned in this Session.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Veronica Gawarecki.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Write code that accomplishes the following (and ONLY the following),
 #   in the order listed:
 #
@@ -43,3 +43,15 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   As always, COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+import rosegraphics as rg
+dan=rg.SimpleTurtle()
+dan.pen=rg.Pen('blue',5)
+dan.left(90)
+dan.forward(200)
+dan.pen_up()
+dan.go_to(rg.Point(100,-40))
+dan.pen_down()
+dan.pen=rg.Pen('green',10)
+dan.backward(150)
+window=rg.TurtleWindow()
+window.close_on_mouse_click()
